c3b8dc8bf7599960ee89afe19e1fc316bdd16778
Elias <you@example.com>
2021-06-01 17:18:36 +0200

    commentaires explicatifs; fonctions

--- Morphologie_mathématique/PARTIE3_SeillageSegmentation.m
+++ Morphologie_mathématique/PARTIE3_SeillageSegmentation.m
@@ -5,15 +5,12 @@ clear all
 close all
 ######################################
 
-
 ImObj = imread('img/OBJECTS.TIF');
-figure(1), imshow(ImObj)
-level = graythresh(ImObj)
-BW = im2bw(ImObj, level);
+figure(1),imshow(ImObj),title('Image originale')   %L'image doit etre bi-modale
+level = graythresh(ImObj)                          %Cette fonction cherche le seuil optimale
+BW = im2bw(ImObj, level);                          %Binarisation de l'image
 figure(2), imshow(BW)
-SQ = bwmorph(BW,'skel',20);
+SQ = bwmorph(BW,'skel',20);                        %Morphologie mat. => "ouverture, fermeture, dilatation, squelettisation, etc, etc..."
 figure(3), imshow(SQ)
 Result = xor(BW,SQ);
 figure(4), imshow(Result)
-
-#Conclusions:
